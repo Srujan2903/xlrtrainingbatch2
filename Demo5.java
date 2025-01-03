08473c8e56b471fc3edfa2bb97bb377a17de6f64
Srujan Dundigalla <srujandundhigala@gmail.com>
2025-01-03 09:52:17 +0530

    while loop
    happy new year

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+public class Demo5 {
+    public static void main (String args[]){
+        String user_name ="user001";
+        String password ="user_001";
+         while((user_name=="user001")&&(password=="user_001")){
+          System.out.println("Hello, Happy New year");
+          break;
+         }          
+    }
+}
