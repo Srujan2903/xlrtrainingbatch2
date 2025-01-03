0ffd8c5347c9775ec44e8dbee90757e1aa1a3127
Srujan Dundigalla <srujandundhigala@gmail.com>
2025-01-03 10:09:06 +0530

    while loop2
    by our names

---
+++ Demo6.java
@@ -0,0 +1,10 @@
+public class Demo6 {
+    public static void main (String args[]){
+         String user_name="Srujan";
+         String password="Srujan@123";         
+         while((user_name=="Srujan")&&(password=="Srujan@123")){
+          System.out.println("Hello Welcome Srujan");
+         break;
+         }         
+    }
+}
