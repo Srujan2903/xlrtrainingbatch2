79f4f1fe89cbaf8d64e6f5b3ada489b7a54885f3
Srujan Dundigalla <srujandundhigala@gmail.com>
2025-01-09 11:35:03 +0530

    minutes to years and hours

--- Demo018.java
+++ Demo018.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Demo018 {
     public static void main(String[] args) {
-        int limit = 10; // Change this value to get more multiples
-        for (int i = 1; i <= limit; i++) {
-            System.out.println(100 * i);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter minutes: ");
+            long minutes = sc.nextLong();
+            
+            long hours = minutes / 60;
+            long minutesInYear = 60 * 24 * 365;
+            long years = minutes / minutesInYear;
+            long remainingMinutes = minutes % minutesInYear;
+            long days = remainingMinutes / (60 * 24);
+            
+            System.out.println(minutes + " minutes is equal to " + hours + " hours.");
+            System.out.println(minutes + " minutes is approximately " + years + " years and " + days + " days.");
         }
     }
 }
