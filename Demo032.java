6c519b1fea5195ec32f4d3a0f4c519419288b8e7
Srujan Dundigalla <srujandundhigala@gmail.com>
2025-01-21 10:19:26 +0530

    Add files via upload
    Homgenues HashSet

---
+++ Demo032.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+public class Demo032 {
+    public static void main(String[] args) {
+        // Create a HashSet to store homogeneous string values
+        HashSet<String> Names = new HashSet<>();
+
+        // Add string values to the HashSet
+        Names.add("Srujan");
+        Names.add("Kowshik");
+        Names.add("Varshith");
+        Names.add("Durga");
+
+        // Display the elements in the HashSet
+        System.out.println(Names);
+    }
+}
