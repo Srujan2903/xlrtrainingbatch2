3beb831334648d3897ddd587975270cb53b651f1
Srujan Dundigalla <srujandundhigala@gmail.com>
2025-02-12 09:53:30 +0530

    Add files via upload

---
+++ test02.java
@@ -0,0 +1,11 @@
+public class test02 {
+    public static void main(String []args){
+        for(int i = 0;i<=100;i++){
+            if(i==98){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
